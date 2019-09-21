6c144b6f2c4f8027120fe47ef3e60f7f818cfc67
Alice Brunel <alc.brunel@gmail.com>
2019-09-21 18:10:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.empty?
-        "Your cart is empty is empty."
+        resp.write "Your cart is empty is empty."
       else 
         @@cart.each do |shopped|
         resp.write "#{shopped}\n"
